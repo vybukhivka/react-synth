d9121a72f1278a825a3891d31b135925c07d57d3
vybukhivka <viktorbakhovka@gmail.com>
2024-11-05 14:46:25 +0000

    router installed, basic router added to App (Main, About, Explore, Error)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,36 @@
+import {
+  createBrowserRouter,
+  RouteObject,
+  RouterProvider,
+} from 'react-router-dom';
+import Main from './pages/Main';
+import PageNotFound from './pages/PageNotFound';
+import About from './pages/About';
+import Explore from './pages/Explore';
+
+const routes: RouteObject[] = [
+  {
+    path: '/',
+    element: <Main />,
+  },
+  {
+    path: '/about',
+    element: <About />,
+  },
+  {
+    path: '/explore',
+    element: <Explore />,
+  },
+  {
+    path: '*',
+    element: <PageNotFound />,
+  },
+];
+
+const router = createBrowserRouter(routes);
+
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;

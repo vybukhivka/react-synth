ace990e8f2adc9b9753d0c8d6f5417b11de75bcc
vybukhivka <viktorbakhovka@gmail.com>
2024-11-06 20:42:18 +0000

    style: react icons + navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,21 +1,26 @@
 import { NavLink } from 'react-router-dom';
+import { FaGithub } from 'react-icons/fa6';
 
 function NavBar() {
   return (
     <nav className="flex h-16 items-center justify-between">
       <div>
         <NavLink to={'/'}>
-          <span className="text font-bold">React Synth</span>
+          <span className="text-lg font-bold">React Synth</span>
         </NavLink>
       </div>
-      <div className="flex justify-between space-x-3">
+      <div className="flex justify-between space-x-8 text-lg">
         <NavLink to={'/about'}>About</NavLink>
         <NavLink to={'/explore'}>Explore</NavLink>
         <NavLink to={'/contact'}>Contact</NavLink>
       </div>
-      <div>
-        <NavLink to={'https://github.com/vybukhivka/react-synth'}>git </NavLink>
-        <NavLink to={'https://github.com/vybukhivka/react-synth'}>inst</NavLink>
+      <div className="flex gap-y-3">
+        <NavLink
+          className="flex items-start justify-start"
+          to={'https://github.com/vybukhivka/react-synth'}
+        >
+          <FaGithub size={30} />
+        </NavLink>
       </div>
     </nav>
   );

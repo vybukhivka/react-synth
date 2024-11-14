457902462881b9499958cf9c08f453844ff7f551
vybukhivka <viktorbakhovka@gmail.com>
2024-11-14 17:17:44 +0000

    sequencer merge

--- src/components/synth/Sequencer/SequncerContainer.tsx
+++ src/components/synth/Sequencer/SequncerContainer.tsx
@@ -1,32 +1,14 @@
-<<<<<<< HEAD
-import SequencerButton from '../../ui/SequencerButton/SequencerButton';
-import SequencerControls from '../Tabs/Main/SequencerControls/SequencerControls';
-=======
 import SequencerRow from './SequencerRow';
->>>>>>> synth-mod-tab
 
-function SequncerContainer() {
+const SequncerContainer: React.FC = () => {
   return (
-<<<<<<< HEAD
-    <>
-      {/* <SequencerControls />
-      mainTab
-       */}
-      <div className="col-start-1 col-end-5 row-start-4 flex items-end justify-between">
-        {steps.map(step => (
-          <SequencerButton key={step} />
-        ))}
-      </div>
-    </>
-=======
     <div className="col-start-1 col-end-5 row-start-3 row-end-5 flex flex-col gap-y-[18px]">
       <SequencerRow />
       <SequencerRow />
       <SequencerRow />
       <SequencerRow />
     </div>
->>>>>>> synth-mod-tab
   );
-}
+};
 
 export default SequncerContainer;

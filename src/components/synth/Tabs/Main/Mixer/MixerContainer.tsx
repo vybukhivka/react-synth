41fa6c547219aa328192aa09efecd14363223a65
vybukhivka <viktorbakhovka@gmail.com>
2024-11-14 11:52:08 +0000

    styles: mixer align

--- src/components/synth/Tabs/Main/Mixer/MixerContainer.tsx
+++ src/components/synth/Tabs/Main/Mixer/MixerContainer.tsx
@@ -4,7 +4,7 @@ import Reverb from './Reverb';
 
 const Mixer: React.FC = () => {
   return (
-    <div className="col-start-5 row-start-2 row-end-4 flex flex-col items-center justify-around">
+    <div className="col-start-5 row-start-2 row-end-4 flex flex-col items-center justify-between gap-y-[20px]">
       <div className="flex items-end justify-around gap-x-4">
         <MixerChannel track={1} />
         <MixerChannel track={2} />
--- src/components/ui/MixerFader/MixerFader.tsx
+++ src/components/ui/MixerFader/MixerFader.tsx
@@ -1,7 +1,7 @@
 const MixerFader: React.FC = () => {
   return (
-    <div className="flex h-[172px] w-[48px] items-end gap-4 overflow-hidden rounded-lg border">
-      <div className="-0 h-[40px] w-full bg-white opacity-20"></div>
+    <div className="flex h-[154px] w-[48px] items-end gap-x-4 overflow-hidden rounded-lg border">
+      <div className="h-[40px] w-full bg-white opacity-20"></div>
     </div>
   );
 };

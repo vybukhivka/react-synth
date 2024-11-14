408c52b13fc2e2c184566fd7d8b43d6d37bca989
vybukhivka <viktorbakhovka@gmail.com>
2024-11-14 11:45:22 +0000

    fix: tracks align

--- src/components/synth/Tracks/TracksContainer.tsx
+++ src/components/synth/Tracks/TracksContainer.tsx
@@ -2,12 +2,14 @@ import Track from './Track';
 
 const TracksContainer: React.FC = () => {
   return (
-    <div className="col-start-1 col-end-5 row-start-1 row-end-3 flex w-[676px] gap-x-5">
-      <Track />
-      <Track />
-      <Track />
-      <Track />
-    </div>
+    <>
+      <div className="col-start-1 col-end-5 row-start-1 row-end-3 flex gap-x-[20px]">
+        <Track />
+        <Track />
+        <Track />
+        <Track />
+      </div>
+    </>
   );
 };
 

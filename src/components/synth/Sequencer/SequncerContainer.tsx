807de5a59f303a03fdb5d09b79b7f5a050b63b9d
vybukhivka <viktorbakhovka@gmail.com>
2024-11-14 16:59:53 +0000

    refactoring: sequncer layout should upadate on tab change

--- src/components/synth/Sequencer/SequncerContainer.tsx
+++ src/components/synth/Sequencer/SequncerContainer.tsx
@@ -1,13 +1,19 @@
 import SequencerButton from '../../ui/SequencerButton/SequencerButton';
+import SequencerControls from '../Tabs/Main/SequencerControls/SequencerControls';
 
 function SequncerContainer() {
   const steps = Array.from({ length: 8 }, (_, i) => i);
   return (
-    <div className="col-start-1 col-end-5 row-start-4 flex items-end justify-between">
-      {steps.map(step => (
-        <SequencerButton key={step} />
-      ))}
-    </div>
+    <>
+      {/* <SequencerControls />
+      mainTab
+       */}
+      <div className="col-start-1 col-end-5 row-start-4 flex items-end justify-between">
+        {steps.map(step => (
+          <SequencerButton key={step} />
+        ))}
+      </div>
+    </>
   );
 }
 

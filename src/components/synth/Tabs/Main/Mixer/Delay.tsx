d889f2b382fb933c62c922ea8cfa7145214b737a
vybukhivka <99396589+vybukhivka@users.noreply.github.com>
2024-11-18 17:36:43 +0200

    main tab colors

--- src/components/synth/Tabs/Main/Mixer/Delay.tsx
+++ src/components/synth/Tabs/Main/Mixer/Delay.tsx
@@ -3,7 +3,7 @@ import MixerSend from '../../../../ui/MixerSend/MixerSend';
 
 const Delay: React.FC = () => {
   return (
-    <div className="flex h-[134px] w-[134px] flex-col items-center justify-end gap-y-2 rounded-3xl border">
+    <div className="flex h-[134px] w-[134px] flex-col items-center justify-end gap-y-2 rounded-3xl border border-slate-600">
       <Knob />
       <div className="flex w-full justify-between p-[16px]">
         <MixerSend />
--- src/components/ui/MixerSend/MixerSend.tsx
+++ src/components/ui/MixerSend/MixerSend.tsx
@@ -1,5 +1,7 @@
 const MixerSend: React.FC = () => {
-  return <div className="h-[20px] w-[48px] rounded-[5px] border"></div>;
+  return (
+    <div className="h-[20px] w-[48px] rounded-[5px] border border-slate-600"></div>
+  );
 };
 
 export default MixerSend;

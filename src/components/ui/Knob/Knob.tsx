7106a86e2c89d1daed30ea5aef7e97c374edee9a
vybukhivka <99396589+vybukhivka@users.noreply.github.com>
2024-11-22 22:01:36 +0200

    redux store, track slice

--- src/components/ui/Knob/Knob.tsx
+++ src/components/ui/Knob/Knob.tsx
@@ -1,6 +1,21 @@
+import clsx from 'clsx';
 import useRotate from '../../../hooks/useRotate';
+import { useAppDispatch, useAppSelector } from '../../../store/hooks';
+import {
+  selectTrackParameter,
+  TrackParameter,
+} from '../../../store/slices/tracksSlice';
+
+type KnobProps = {
+  trackId: number;
+  param: keyof TrackParameter;
+};
 
 const Knob: React.FC = () => {
+  const dispatch = useAppDispatch();
+  const value = useAppSelector(state =>
+    selectTrackParameter(state, 'track1', 'param2'),
+  );
   const { elementRef: knob, angle, startRotate } = useRotate();
 
   return (
@@ -8,7 +23,7 @@ const Knob: React.FC = () => {
       <div
         ref={knob}
         onMouseDown={startRotate}
-        style={{ transform: `rotate(${angle}deg)`, userSelect: 'none' }}
+        style={{ transform: `rotate(${value}deg)`, userSelect: 'none' }}
         className="flex h-[40px] w-[40px] origin-center items-center justify-start rounded-full border border-slate-400 bg-transparent p-[6px]"
       >
         <div className="h-[2px] w-[5px] rounded-lg border bg-white"></div>

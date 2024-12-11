e33a0fbbfa0707698bed07b5e3be32e8655592a7
vybukhivka <viktorbakhovka@gmail.com>
2024-12-11 20:22:25 +0200

    refactoring: better types, new selector and separated filter for channels

--- src/components/synth/Tabs/Main/Mixer/MixerContainer.tsx
+++ src/components/synth/Tabs/Main/Mixer/MixerContainer.tsx
@@ -1,26 +1,33 @@
 import { useAppSelector } from '../../../../../store/hooks';
-import { selectMixer } from '../../../../../store/slices/mixerSlice';
+import { selectMixerChannels } from '../../../../../store/slices/mixerSlice';
 import Delay from './Delay';
 import MixerChannel from './MixerChannel';
 import Reverb from './Reverb';
 
-const BORDERS = ['bg-purple-400', 'bg-indigo-400', 'bg-sky-400', 'bg-teal-400'];
+const BACKGROUNDS: Record<string, string> = {
+  track1: 'bg-purple-400',
+  track2: 'bg-indigo-400',
+  track3: 'bg-sky-400',
+  track4: 'bg-teal-400',
+};
 
 const Mixer: React.FC = () => {
-  const mixerState = useAppSelector(selectMixer);
-  const mixerData = Object.entries(mixerState);
+  const mixerChannels = useAppSelector(selectMixerChannels);
+  const filteredChannels = Object.entries(mixerChannels).filter(
+    ([trackId]) => trackId !== 'delay' && trackId !== 'reverb',
+  );
 
   return (
     <div className="col-start-5 row-start-2 row-end-5 flex h-full flex-col items-center justify-between gap-y-[20px] px-[20px]">
       <div className="flex items-end justify-between gap-x-5">
-        {mixerData &&
-          mixerData.map((track, i) => {
-            if (track[0] === 'delay') return;
-            if (track[0] === 'reverb') return;
-            return (
-              <MixerChannel className={BORDERS[i]} trackId={track[0]} key={i} />
-            );
-          })}
+        {filteredChannels &&
+          filteredChannels.map(([trackId], i) => (
+            <MixerChannel
+              className={BACKGROUNDS[trackId] || 'bg-slate-400'}
+              trackId={trackId}
+              key={i}
+            />
+          ))}
       </div>
       <div className="flex justify-center gap-x-5">
         <Delay />

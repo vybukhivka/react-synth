2b4017d50d6da99891dc423692cecc42e6fe2324
vybukhivka <viktorbakhovka@gmail.com>
2024-12-16 22:51:16 +0200

    mixer fx redux setup

--- src/components/synth/Tabs/Main/Mixer/MixerFx.tsx
+++ src/components/synth/Tabs/Main/Mixer/MixerFx.tsx
@@ -1,4 +1,8 @@
-import { MixerState } from '../../../../../store/slices/mixerSlice';
+import { useAppSelector } from '../../../../../store/hooks';
+import {
+  MixerState,
+  selectMixerFx,
+} from '../../../../../store/slices/mixerSlice';
 import Knob from '../../../../ui/Knob/Knob';
 import MixerSend from '../../../../ui/MixerSend/MixerSend';
 
@@ -6,10 +10,16 @@ type MixerFxProps = {
   fxName: keyof MixerState['returnFx'];
 };
 const MixerFx: React.FC<MixerFxProps> = ({ fxName }) => {
+  const mixerReturnFx = useAppSelector(selectMixerFx);
+  const time = mixerReturnFx[fxName].time;
+  const paramName: 'time' | 'decay' | null =
+    fxName === 'reverb' ? 'decay' : fxName === 'delay' ? 'time' : null;
+  console.log(paramName);
+
   return (
     <div className="flex h-[134px] w-[134px] flex-col items-center justify-end gap-y-2 rounded-3xl border border-slate-600">
       <span>{fxName}</span>
-      <Knob paramValue={35} paramName="time" />
+      <Knob paramValue={time} />
       <div className="flex w-full justify-between p-[16px]">
         <MixerSend type="damp" />
         <MixerSend type="feedback" />

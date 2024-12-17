94c61d1d2ff276f40c07b6aac9b13c2e77ae8e6c
vybukhivka <viktorbakhovka@gmail.com>
2024-12-17 19:19:11 +0200

    utility function for updating draggable elements

---
+++ src/utils/updateDraggable.ts
@@ -0,0 +1,28 @@
+import { DragElement } from '../types/dragTypes';
+import clamp from './clamp';
+
+export default function updateDraggable(
+  type: DragElement,
+  angle: number,
+  deltaX: number,
+  deltaY: number,
+  onUpdate: (value: number) => void,
+) {
+  switch (type) {
+    case 'knob': {
+      const newAngle = clamp(angle + deltaX + deltaY, -45, 225);
+      onUpdate(newAngle);
+      break;
+    }
+    case 'fader': {
+      const newHeight = clamp(angle + deltaY, 0, 154);
+      onUpdate(newHeight);
+      break;
+    }
+    case 'send': {
+      const newWidth = clamp(angle + deltaX, 0, 48);
+      onUpdate(newWidth);
+      break;
+    }
+  }
+}

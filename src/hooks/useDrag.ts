58b0797a75219ee4f011f7b42d45473abbdc43a8
vybukhivka <viktorbakhovka@gmail.com>
2024-12-17 19:21:28 +0200

    refactoring: utility fucntions handle state updates, including redux

--- src/hooks/useDrag.ts
+++ src/hooks/useDrag.ts
@@ -3,27 +3,32 @@ import { useAppDispatch } from '../store/hooks';
 import {
   TrackParams,
   TrackState,
-  updateParameter,
+  updateTrackParameter,
 } from '../store/slices/tracksSlice';
 import { DragElement } from '../types/dragTypes';
 import angleToValue from '../utils/angleToValue';
 import valueToAngle from '../utils/valueToAngle';
-import { updateFader } from '../store/slices/mixerSlice';
+import {
+  MixerChannelParams,
+  MixerState,
+  updateMixerParameter,
+} from '../store/slices/mixerSlice';
 import { setActiveType } from '../components/synth/Tracks/Track';
 import calcDelta from '../utils/calcDelta';
 import clamp from '../utils/clamp';
+import updateDraggable from '../utils/updateDraggable';
 
 type UseDragResults = {
   elementRef: React.MutableRefObject<HTMLDivElement | null>;
   angle: number;
-  startDrag: (e: MouseEvent) => void;
+  startDrag: (e: React.MouseEvent) => void;
 };
 
 type UseDragProps = {
   initialValue: number;
   type: DragElement;
-  trackId: keyof TrackState;
-  paramName: keyof TrackParams;
+  trackId: keyof TrackState | keyof MixerState;
+  paramName: keyof TrackParams | keyof MixerChannelParams;
   setActiveParam: React.Dispatch<React.SetStateAction<setActiveType>>;
 };
 
@@ -40,34 +45,14 @@ function useDrag({
   const elementRef = useRef<HTMLDivElement | null>(null);
   const [isDragging, setIsDragging] = useState(false);
 
-  // 1. Calculate delta +
-  // 2. Clamp values +
-  // 3. Type based updates
-
   function handleMove(e: MouseEvent) {
     if (!isDragging || !initialPosition.current || !elementRef.current) return;
 
     const { deltaX, deltaY } = calcDelta(e, initialPosition.current);
 
-    if (type === 'knob' && setActiveParam && paramName) {
-      const deltaSum = deltaX + deltaY;
-      let newAngle = angle + deltaSum;
-      newAngle = clamp(newAngle, -45, 225);
-      setAngle(newAngle);
-    }
-
-    if (type === 'fader') {
-      let newHeight = angle + deltaY;
-      newHeight = clamp(newHeight, 0, 154);
-      setAngle(newHeight);
-    }
-
-    if (type === 'send') {
-      let newWidth = angle + deltaX;
-      newWidth = Math.max(0, Math.min(newWidth, 48));
-      newWidth = clamp(newWidth, 0, 48);
-      setAngle(newWidth);
-    }
+    updateDraggable(type, angle, deltaX, deltaY, newValue =>
+      setAngle(newValue),
+    );
   }
 
   function startDrag(e: MouseEvent) {
@@ -80,42 +65,25 @@ function useDrag({
     if (!initialPosition.current) return;
     const { deltaX, deltaY } = calcDelta(e, initialPosition.current);
 
-    if (type === 'knob' && trackId && paramName) {
-      const deltaSum = deltaX + deltaY;
-      let newAngle = angle + deltaSum;
-      newAngle = Math.max(-45, Math.min(newAngle, 225));
-      dispatch(
-        updateParameter({
-          trackId,
-          paramName,
-          paramValue: angleToValue(newAngle, 'knob'),
-        }),
-      );
-    }
-
-    if (type === 'fader' && trackId && paramName) {
-      let newHeight = angle + deltaY;
-      newHeight = Math.max(0, Math.min(newHeight, 154));
-      dispatch(
-        updateFader({
-          trackId,
-          paramName: 'volume',
-          paramValue: angleToValue(newHeight, type),
-        }),
-      );
-    }
-
-    if (type === 'send' && trackId && paramName) {
-      let newWidth = angle + deltaX;
-      newWidth = Math.max(0, Math.min(newWidth, 48));
-      dispatch(
-        updateFader({
-          trackId,
-          paramName: String(paramName),
-          paramValue: angleToValue(newWidth, type),
-        }),
-      );
-    }
+    updateDraggable(type, angle, deltaX, deltaY, value => {
+      if (type === 'knob') {
+        dispatch(
+          updateTrackParameter({
+            trackId,
+            paramName,
+            paramValue: angleToValue(value, type),
+          }),
+        );
+      } else {
+        dispatch(
+          updateMixerParameter({
+            trackId,
+            paramName,
+            paramValue: angleToValue(value, type),
+          }),
+        );
+      }
+    });
 
     initialPosition.current = { x: e.clientX, y: e.clientY };
   }

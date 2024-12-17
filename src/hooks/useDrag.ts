9d9b9637fc0320b43544870592a8f488b18f8ba7
vybukhivka <viktorbakhovka@gmail.com>
2024-12-17 17:41:37 +0200

    refactoring: clamp values with utility function

--- src/hooks/useDrag.ts
+++ src/hooks/useDrag.ts
@@ -11,6 +11,7 @@ import valueToAngle from '../utils/valueToAngle';
 import { updateFader } from '../store/slices/mixerSlice';
 import { setActiveType } from '../components/synth/Tracks/Track';
 import calcDelta from '../utils/calcDelta';
+import clamp from '../utils/clamp';
 
 type UseDragResults = {
   elementRef: React.MutableRefObject<HTMLDivElement | null>;
@@ -40,7 +41,7 @@ function useDrag({
   const [isDragging, setIsDragging] = useState(false);
 
   // 1. Calculate delta +
-  // 2. Clamp values
+  // 2. Clamp values +
   // 3. Type based updates
 
   function handleMove(e: MouseEvent) {
@@ -51,19 +52,20 @@ function useDrag({
     if (type === 'knob' && setActiveParam && paramName) {
       const deltaSum = deltaX + deltaY;
       let newAngle = angle + deltaSum;
-      newAngle = Math.max(-45, Math.min(newAngle, 225));
+      newAngle = clamp(newAngle, -45, 225);
       setAngle(newAngle);
     }
 
     if (type === 'fader') {
       let newHeight = angle + deltaY;
-      newHeight = Math.max(0, Math.min(newHeight, 154));
+      newHeight = clamp(newHeight, 0, 154);
       setAngle(newHeight);
     }
 
     if (type === 'send') {
       let newWidth = angle + deltaX;
       newWidth = Math.max(0, Math.min(newWidth, 48));
+      newWidth = clamp(newWidth, 0, 48);
       setAngle(newWidth);
     }
   }

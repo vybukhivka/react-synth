94956b753b39bfc3fda7985947556ceb2d9bb9a8
vybukhivka <viktorbakhovka@gmail.com>
2024-12-18 18:20:53 +0200

    redux: modulation cell values coming from store and arranged with utility function

--- src/components/synth/Tabs/Mod/Matrix.tsx
+++ src/components/synth/Tabs/Mod/Matrix.tsx
@@ -1,52 +1,35 @@
-import useDrag from '../../../../hooks/useDrag/useDrag';
 import { useAppSelector } from '../../../../store/hooks';
 import {
   ModulationState,
   selectMatrix,
 } from '../../../../store/slices/modulationSlice';
-import angleToValue from '../../../../utils/angleToValue';
+import ModCell from '../../../ui/ModCell/ModCell';
 
-const matrixSlots: number[] = Array.from({ length: 16 }, (_, i) => i);
+// const matrixSlots: number[] = Array.from({ length: 16 }, (_, i) => i);
 
-function readMatrixValues(track: keyof ModulationState, matrix) {
-  const test = Object.entries(matrix[track]);
-  console.log(test);
+function readMatrixValues(
+  track: keyof ModulationState,
+  matrix: ModulationState,
+) {
+  const modMatirx = Object.entries(matrix[track]);
   // 1. loop over each mod source
+  const trackMods = modMatirx.map(source => {
+    const values = Object.values(source[1]);
+    return values;
+  });
   // 2. push values of each track to output array
+  return trackMods.flat();
 }
 
 const Matrix: React.FC = ({ selectedTrack }) => {
   const matrix = useAppSelector(selectMatrix);
-  readMatrixValues('track1', matrix);
+  const matrixSlots = readMatrixValues('track1', matrix);
+  // console.log(arr);
 
-  const {
-    elementRef: modMatrixCell,
-    angle,
-    startDrag,
-  } = useDrag({
-    initialValue: 20,
-    type: 'modMatrixCell',
-  });
   return (
     <div className="grid h-[200px] w-[200px] grid-cols-4 grid-rows-4 border-slate-600">
       {matrixSlots.map(slot => (
-        <div
-          onMouseDown={startDrag}
-          className="relative border border-slate-600 text-center"
-          key={slot}
-        >
-          <div
-            ref={modMatrixCell}
-            style={{
-              height: `${Math.abs(angle)}px`,
-              transform: `translateY(${angle >= 0 ? 0 : 100}%)`,
-              userSelect: 'none',
-            }}
-            className="absolute bottom-1/2 left-0 w-full bg-slate-600 opacity-50"
-          >
-            {angleToValue(angle, 'modMatrixCell')}
-          </div>
-        </div>
+        <ModCell key={slot} />
       ))}
     </div>
   );

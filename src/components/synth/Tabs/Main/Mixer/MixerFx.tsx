d0f0de8ad082c6309bb5164d348bdeb1cc796223
vybukhivka <viktorbakhovka@gmail.com>
2024-12-18 03:25:54 +0200

    refactoring: early return if no fx params and values

--- src/components/synth/Tabs/Main/Mixer/MixerFx.tsx
+++ src/components/synth/Tabs/Main/Mixer/MixerFx.tsx
@@ -13,29 +13,28 @@ type MixerFxProps = {
 
 const MixerFx: React.FC<MixerFxProps> = ({ fxName }) => {
   const mixerReturnFx = useAppSelector(selectMixerFx);
-  const {
-    knobName,
-    firstParamName,
-    secondParamName,
-    knobValue,
-    firstParamValue,
-    secondParamValue,
-  } = defineMixerFx(fxName, mixerReturnFx);
+  const mixerFx = defineMixerFx(fxName, mixerReturnFx);
+  if (!mixerFx)
+    throw new Error('MixerFx: undefined returned from defineMixerFx');
 
   return (
     <div className="flex h-[134px] w-[134px] flex-col items-center justify-end gap-y-2 rounded-3xl border border-slate-600">
       <span>{fxName}</span>
-      <Knob fxName={fxName} paramName={knobName} paramValue={knobValue} />
+      <Knob
+        fxName={fxName}
+        paramName={mixerFx.knobName}
+        paramValue={mixerFx.knobValue}
+      />
       <div className="flex w-full justify-between p-[16px]">
         <MixerSend
           fxName={fxName}
-          paramName={firstParamName}
-          paramValue={firstParamValue}
+          paramName={mixerFx.firstParamName}
+          paramValue={mixerFx.firstParamValue}
         />
         <MixerSend
           fxName={fxName}
-          paramName={secondParamName}
-          paramValue={secondParamValue}
+          paramName={mixerFx.secondParamName}
+          paramValue={mixerFx.secondParamValue}
         />
       </div>
     </div>

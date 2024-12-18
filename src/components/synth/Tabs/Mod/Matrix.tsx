985b7fd1f5880d4b4cbe65f8e3b15509de5e53d8
vybukhivka <viktorbakhovka@gmail.com>
2024-12-18 16:11:21 +0200

    matrix cells with bipolar adjustemets, each cell shows its value (currently in px)

--- src/components/synth/Tabs/Mod/Matrix.tsx
+++ src/components/synth/Tabs/Mod/Matrix.tsx
@@ -4,25 +4,32 @@ const matrixSlots: number[] = Array.from({ length: 16 }, (_, i) => i);
 
 const Matrix: React.FC = () => {
   const {
-    elementRef: modCell,
+    elementRef: modMatrixCell,
     angle,
     startDrag,
   } = useDrag({
     initialValue: 10,
+    type: 'modMatrixCell',
   });
   return (
     <div className="grid h-[200px] w-[200px] grid-cols-4 grid-rows-4 border-slate-600">
       {matrixSlots.map(slot => (
         <div
           onMouseDown={startDrag}
-          className="border border-slate-600 text-center"
+          className="relative border border-slate-600 text-center"
           key={slot}
         >
           <div
-            ref={modCell}
-            style={{ height: `${angle}px`, userSelect: 'none' }}
-            className="w-full bg-slate-600 opacity-50"
-          ></div>
+            ref={modMatrixCell}
+            style={{
+              height: `${Math.abs(angle)}px`,
+              transform: `translateY(${angle >= 0 ? 0 : 100}%)`,
+              userSelect: 'none',
+            }}
+            className="absolute bottom-1/2 left-0 w-full bg-slate-600 opacity-50"
+          >
+            {angle}
+          </div>
         </div>
       ))}
     </div>

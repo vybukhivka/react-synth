8f853cd66acee895d91dfc46895d07aa4409951f
vybukhivka <viktorbakhovka@gmail.com>
2024-12-20 23:50:58 +0200

    selectors and props for modulators components

--- src/components/synth/Tabs/Mod/LFO.tsx
+++ src/components/synth/Tabs/Mod/LFO.tsx
@@ -1,11 +1,20 @@
+import { useAppSelector } from '../../../../store/hooks';
+import { selectSources } from '../../../../store/slices/modulationSlice';
 import MixerSend from '../../../ui/MixerSend/MixerSend';
 
 const LFO: React.FC = () => {
+  const sources = useAppSelector(selectSources);
+  const freq = sources.LFO.freq;
   return (
     <div className="flex h-[134px] w-[134px] flex-col items-center justify-end gap-y-2 rounded-3xl border border-slate-600">
       LFO wave
       <div className="flex w-full justify-center p-[16px]">
-        <MixerSend />
+        <MixerSend
+          type="modulator"
+          fxName="LFO"
+          paramName="freq"
+          paramValue={freq}
+        />
       </div>
     </div>
   );

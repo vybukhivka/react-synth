39e9b1de70c3938ef63e215bae524eb0427fb384
vybukhivka <viktorbakhovka@gmail.com>
2024-12-29 03:14:31 +0200

    utility function for deriving a sequencer state (probably dont need that)

--- src/components/synth/Sequencer/SequencerRow.tsx
+++ src/components/synth/Sequencer/SequencerRow.tsx
@@ -1,19 +1,45 @@
+import { useAppSelector } from '../../../store/hooks';
+import {
+  selectSequencer,
+  SequencerState,
+  TrackSequencerState,
+} from '../../../store/slices/sequencerSlice';
 import SequencerButton from '../../ui/SequencerButton/SequencerButton';
 
 type SequencerRowProps = {
   className: string;
+  track: keyof SequencerState;
 };
 
-const steps = Array.from({ length: 8 }, (_, i) => ({
-  id: i,
-  isTriggered: false,
-}));
+const readSequencerValues = (state: TrackSequencerState) => {
+  const trigs = state.trigs;
+  const probability = state.probability;
+  const velocity = state.velocity;
+  const retrig = state.retrig;
+  const modSequence = state.modSequence;
 
-const SequencerRow: React.FC<SequencerRowProps> = ({ className }) => {
+  return trigs.map((trig, index) => ({
+    trigs: trig,
+    probability: probability[index],
+    velocity: velocity[index],
+    retrig: retrig[index],
+    modSequence: modSequence[index],
+  }));
+};
+
+const SequencerRow: React.FC<SequencerRowProps> = ({ className, track }) => {
+  const state = useAppSelector(selectSequencer);
+  const steps = readSequencerValues(state[track]);
   return (
     <div className="flex items-end justify-between">
-      {steps.map(step => (
-        <SequencerButton color={className} key={step.id} />
+      {steps.map((step, i) => (
+        <SequencerButton
+          isTriggered={step.trigs}
+          color={className}
+          step={i}
+          track={track}
+          key={`step${i + 1}`}
+        />
       ))}
     </div>
   );

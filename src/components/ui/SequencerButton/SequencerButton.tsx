7b4125e61126cfcd8395e0e24ac0d4614c136504
vybukhivka <viktorbakhovka@gmail.com>
2024-12-29 03:15:11 +0200

    sequencer button props, types and redux reducer usage

--- src/components/ui/SequencerButton/SequencerButton.tsx
+++ src/components/ui/SequencerButton/SequencerButton.tsx
@@ -1,17 +1,51 @@
 import { useState } from 'react';
 import { cn } from '../../../utils/cn';
+import { useAppDispatch } from '../../../store/hooks';
+import {
+  SequencerState,
+  updateTrackProperty,
+} from '../../../store/slices/sequencerSlice';
 
 type SequencerButtonProps = {
   color: string;
-  // isTriggered: boolean;
-  // isActive: boolean
+  isTriggered: boolean;
+  retrig: number;
+  modSequence: number;
+  probability: number;
+  velocity: number;
+  step: number;
+  track: keyof SequencerState;
 };
 
-const SequencerButton: React.FC<SequencerButtonProps> = ({ color }) => {
-  const [trigger, setTrigger] = useState(false);
+const SequencerButton: React.FC<Partial<SequencerButtonProps>> = ({
+  color,
+  isTriggered,
+  step,
+  track,
+}) => {
+  const [trigger, setTrigger] = useState(isTriggered);
+  const dispatch = useAppDispatch();
+  // trackId: keyof SequencerState;
+  // property: keyof TrackSequencerState;
+  // step: number;
+  // value: number | string | boolean;
+
+  const handleTrigger = () => {
+    console.log(step);
+    setTrigger(!trigger);
+    dispatch(
+      updateTrackProperty({
+        trackId: track,
+        step,
+        property: 'trigs',
+        value: !trigger,
+      }),
+    );
+  };
+
   return (
     <button
-      onClick={() => setTrigger(!trigger)}
+      onClick={handleTrigger}
       className={cn(
         'flex h-[68px] w-[68px] items-center justify-center rounded-lg border opacity-60 hover:opacity-80',
         color,

edd19409c60ac85510a1ab6e2640543a2e5f4476
vybukhivka <viktorbakhovka@gmail.com>
2024-12-29 03:13:51 +0200

    props for sequencer rows

--- src/components/synth/Sequencer/SequencerContainer.tsx
+++ src/components/synth/Sequencer/SequencerContainer.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { SynthTabs } from '../../../types/synthTabs';
 import SequencerRow from './SequencerRow';
 
@@ -6,27 +7,28 @@ type SeqContainerProps = {
 };
 
 const SequencerContainer: React.FC<SeqContainerProps> = ({ activeTab }) => {
+  const [activeTrack] = useState('track1');
   return (
     <>
       {activeTab === 'main' && (
         <div className="col-start-1 col-end-5 row-start-5 row-end-5 flex flex-col gap-y-[18px]">
-          <SequencerRow className="border-purple-400" />
+          <SequencerRow track={activeTrack} className="border-purple-400" />
         </div>
       )}
       {activeTab === 'seq' && (
         <div className="col-start-1 col-end-5 row-start-3 row-end-5 flex flex-col gap-y-[18px]">
-          <SequencerRow className="border-purple-400" />
-          <SequencerRow className="border-indigo-400" />
-          <SequencerRow className="border-sky-400" />
-          <SequencerRow className="border-teal-400" />
+          <SequencerRow track={'track1'} className="border-purple-400" />
+          <SequencerRow track={'track2'} className="border-indigo-400" />
+          <SequencerRow track={'track3'} className="border-sky-400" />
+          <SequencerRow track={'track4'} className="border-teal-400" />
         </div>
       )}
       {activeTab === 'mod' && (
         <div className="col-start-1 col-end-5 row-start-3 row-end-5 flex flex-col gap-y-[18px]">
-          <SequencerRow className="border-purple-400" />
-          <SequencerRow className="border-indigo-400" />
-          <SequencerRow className="border-sky-400" />
-          <SequencerRow className="border-teal-400" />
+          <SequencerRow track={'track1'} className="border-purple-400" />
+          <SequencerRow track={'track2'} className="border-indigo-400" />
+          <SequencerRow track={'track3'} className="border-sky-400" />
+          <SequencerRow track={'track4'} className="border-teal-400" />
         </div>
       )}
     </>

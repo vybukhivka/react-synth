0e0a96b97d6e240c799d548c7afc62bd3c14c4a7
vybukhivka <viktorbakhovka@gmail.com>
2024-12-30 17:42:13 +0200

    tone: playback button starts transport

--- src/components/ui/PlaybackButton/PlaybackButton.tsx
+++ src/components/ui/PlaybackButton/PlaybackButton.tsx
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import * as Tone from 'tone';
+import { createDummySynth } from '../../../utils/toneHelper';
 
 type PlaybackButtonProps = {
   text: string;
 };
 
 const PlaybackButton: React.FC<PlaybackButtonProps> = props => {
+  const synth = createDummySynth();
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const handlePlayback = () => {
+    setIsPlaying(!isPlaying);
+  };
+
+  useEffect(() => {
+    if (isPlaying) Tone.getTransport().start();
+    else Tone.getTransport().stop();
+  }, [isPlaying]);
+
   return (
     <button
       className="h-[68px] w-[68px] rounded-lg border border-slate-600"
       aria-label={props.text}
+      onClick={handlePlayback}
     >
       {props.text}
     </button>

8fc24997bc36b255004a4923ed948d42ca953df6
vybukhivka <viktorbakhovka@gmail.com>
2024-12-30 14:51:05 +0200

    type from dummy synth

--- src/utils/toneHelper.ts
+++ src/utils/toneHelper.ts
@@ -1,6 +1,6 @@
 import * as Tone from 'tone';
 
-export const createDummySynth = () => {
+export const createDummySynth = (): Tone.Synth => {
   const synth = new Tone.Synth().toDestination();
   return synth;
 };

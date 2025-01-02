26c9c7851c112dccfbb3aea9863c85232b69d392
vybukhivka <viktorbakhovka@gmail.com>
2025-01-02 23:22:36 +0200

    dummy synth plays kick patch

--- src/utils/toneHelper.ts
+++ src/utils/toneHelper.ts
@@ -1,6 +1,10 @@
 import * as Tone from 'tone';
 
 export const createDummySynth = (): Tone.Synth => {
-  const synth = new Tone.Synth().toDestination();
+  const synth = new Tone.Synth({
+    oscillator: { type: 'sine' },
+    envelope: { attack: 0.01, decay: 0.2, sustain: 0, release: 0.2 },
+    volume: -10,
+  }).toDestination();
   return synth;
 };

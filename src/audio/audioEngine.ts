5d2eb83121859d1d04b6327a378482c4b515ae02
vybukhivka <viktorbakhovka@gmail.com>
2025-01-03 18:56:09 +0200

    correct bpm logic, synths plays on triggered step, sequencer direction utility fucntion

--- src/audio/audioEngine.ts
+++ src/audio/audioEngine.ts
@@ -1,12 +1,67 @@
 import * as Tone from 'tone';
+import store from '../store/store';
+import {
+  SequencerState,
+  TrackSequencerState,
+  updateNonArrayTrackProperty,
+} from '../store/slices/sequencerSlice';
 
 const isDebugMode = import.meta.env.MODE === 'development';
+let pingPingDirection: 'forward' | 'backward' = 'forward';
+
+const getNextStepIndex = (
+  trackState: TrackSequencerState,
+  trackId: keyof SequencerState,
+): number => {
+  const { length, direction } = trackState;
+  let nextStep: number = 1;
+  const currentStep = trackState.currentStep || 0;
+
+  if (direction === 'forward') nextStep = (currentStep + 1) % length;
+
+  if (direction === 'backward') {
+    nextStep = currentStep - 1;
+    if (nextStep === -1) nextStep = length;
+  }
+
+  if (direction === 'forwardBackward') {
+    if (pingPingDirection === 'forward' && currentStep + 1 !== length)
+      nextStep = currentStep + 1;
+    else if (currentStep + 1 === length) pingPingDirection = 'backward';
+    if (pingPingDirection === 'backward' && currentStep - 1 !== 0)
+      nextStep = currentStep - 1;
+    else if (currentStep - 1 === 0 && pingPingDirection === 'backward') {
+      pingPingDirection = 'forward';
+      nextStep = 0;
+    }
+    console.log(pingPingDirection, nextStep);
+  }
+
+  store.dispatch(
+    updateNonArrayTrackProperty({
+      trackId,
+      property: 'currentStep',
+      value: nextStep,
+    }),
+  );
+  return nextStep;
+};
 
 export const audioEngine = {
   synths: Array.from({ length: 4 }, () => new Tone.Synth().toDestination()),
 
   transport: Tone.getTransport(),
   clock: new Tone.Clock(time => {
+    const state = store.getState().sequencer;
+
+    Object.entries(state).forEach(([trackId, trackState]) => {
+      const stepIndex = getNextStepIndex(trackState, trackId);
+      if (trackState.trigs[stepIndex]) {
+        const synthIndex = parseInt(trackId.replace('track', ''), 10) - 1;
+        audioEngine.synths[synthIndex].triggerAttackRelease('C2', '16n', time);
+      }
+    });
+
     if (isDebugMode) console.log(`Clock ticked at: ${time}`);
   }, 2),
 
@@ -18,7 +73,9 @@ export const audioEngine = {
   startClock: () => {
     if (audioEngine.clock.state !== 'started') {
       audioEngine.clock.start();
-      if (isDebugMode) console.log('Clock Started');
+      if (isDebugMode) {
+        console.log('Clock Started');
+      }
     }
   },
   stopClock: () => {
@@ -29,7 +86,7 @@ export const audioEngine = {
   },
 
   setBPM: (bpm: number) => {
-    audioEngine.clock.frequency.value = bpm / 60;
+    audioEngine.clock.frequency.value = (bpm / 60) * 4;
     if (isDebugMode) console.log(`BPM set to ${bpm}`);
   },
 

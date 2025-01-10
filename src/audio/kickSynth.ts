8a69cb5d00e7f6c00e85dbe76d4e6178a9a61903
vybukhivka <viktorbakhovka@gmail.com>
2025-01-11 00:47:55 +0200

    second oscillator, setDecay, setPitchEnv

--- src/audio/kickSynth.ts
+++ src/audio/kickSynth.ts
@@ -1,31 +1,41 @@
 import * as Tone from 'tone';
 import store from '../store/store';
 
+// dec env
+// pitch env
+// bitcrusher
+
 export const createKickSynth = () => {
   const state = store.getState().tracks;
 
-  const oscillator = new Tone.Oscillator({
+  const oscillator1 = new Tone.Oscillator({
     type: 'sine',
     frequency: state.track1.param1,
     volume: -12,
+  });
+
+  const oscillator2 = new Tone.Oscillator({
+    type: 'sine',
+    frequency: state.track1.param1 + 50,
+    volume: -13,
   });
 
   const noise = new Tone.Noise({ type: 'pink', volume: -12 });
 
   const amplitudeEnvelope = new Tone.AmplitudeEnvelope({
     attack: 0.005,
-    decay: 0.3,
+    decay: state.track1.param1,
     sustain: 0,
-    release: 0.3,
+    release: state.track1.param1,
   });
 
   const pitchEnvelope = new Tone.FrequencyEnvelope({
     attack: 0.005,
-    decay: 0.1,
+    decay: 0.01,
     sustain: 0,
-    release: 0.1,
+    release: 0.01,
     baseFrequency: state.track1.param1,
-    octaves: 3,
+    octaves: 1.5,
   });
 
   const filter = new Tone.Filter({
@@ -34,13 +44,15 @@ export const createKickSynth = () => {
     rolloff: -24,
   });
 
-  pitchEnvelope.connect(oscillator.frequency);
-  oscillator.connect(filter);
+  pitchEnvelope.connect(oscillator1.frequency);
+  oscillator1.connect(filter);
+  oscillator2.connect(filter);
   noise.connect(filter);
   filter.connect(amplitudeEnvelope);
   amplitudeEnvelope.toDestination();
 
-  oscillator.start();
+  oscillator1.start();
+  oscillator2.start();
   noise.start();
 
   const trigger = (time = Tone.now()) => {
@@ -54,19 +66,33 @@ export const createKickSynth = () => {
 
   const setFrequencer = (frequency: number) => {
     pitchEnvelope.baseFrequency = frequency;
-    oscillator.frequency.value = frequency;
+    oscillator1.frequency.value = frequency;
     console.log('frequency set: ', frequency);
   };
 
+  const setDecay = (time: number) => {
+    amplitudeEnvelope.decay = time;
+    amplitudeEnvelope.release = time;
+    console.log('dec set: ', time);
+  };
+
+  const setPitchEnv = (time: number) => {
+    pitchEnvelope.decay = time;
+    pitchEnvelope.release = time;
+    console.log('dec set: ', time);
+  };
+
   const dispose = () => {
-    oscillator.stop();
+    oscillator1.stop();
+    oscillator2.stop();
     noise.stop();
-    oscillator.dispose();
+    oscillator1.dispose();
+    oscillator2.dispose();
     noise.dispose();
     amplitudeEnvelope.dispose();
     pitchEnvelope.dispose();
     filter.dispose();
   };
 
-  return { trigger, setFrequencer, dispose };
+  return { trigger, setPitchEnv, setDecay, setFrequencer, dispose };
 };

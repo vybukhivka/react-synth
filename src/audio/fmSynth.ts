4ffa6621a458ce5e79a062d242278a0c1319006d
vybukhivka <viktorbakhovka@gmail.com>
2025-01-12 02:02:02 +0200

    initial fm synth

---
+++ src/audio/fmSynth.ts
@@ -0,0 +1,36 @@
+import * as Tone from 'tone';
+import store from '../store/store';
+
+export const createFmSynth = () => {
+  const state = store.getState().tracks;
+
+  const synth = new Tone.FMSynth({
+    volume: -12,
+    detune: 50,
+    modulationIndex: 25,
+    modulationEnvelope: {
+      attack: 0.005,
+      decay: 0.1,
+      sustain: 0,
+      release: 0.1,
+    },
+    envelope: {
+      attack: 0.005,
+      decay: 0.25,
+      sustain: 0,
+      release: 0.25,
+    },
+  });
+  synth.toDestination();
+
+  const trigger = (time = Tone.now()) => {
+    synth.triggerAttackRelease('c2', '16n', time);
+  };
+
+  const dispose = () => {
+    synth.dispose();
+    // gain.dispose();
+  };
+
+  return { trigger, synth, dispose };
+};

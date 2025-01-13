3a62d53957b28a59b7803b980c8bc6b548a5ffac
vybukhivka <viktorbakhovka@gmail.com>
2025-01-13 17:48:11 +0200

    refactored render in Matrix

--- src/components/synth/Tabs/Mod/Matrix.tsx
+++ src/components/synth/Tabs/Mod/Matrix.tsx
@@ -5,27 +5,33 @@ import ModCell from '../../../ui/ModCell/ModCell';
 
 const Matrix: React.FC = ({ selectedTrack }) => {
   const matrix = useAppSelector(selectMatrix);
-  const modCells = readMatrixValues('track1', matrix);
+  const modCells = readMatrixValues('track1', matrix); // TODO: hardcoded track
+  const rowLables = ['P1', 'P2', 'P3', 'P4'];
+  const colLables = ['LFO', 'RND', 'SEQ', 'VEL'];
 
   return (
     <div className="mr-9 grid h-[250px] w-[250px] grid-cols-5 grid-rows-5 border-slate-600">
-      <span className="col-start-1 row-start-2 content-center justify-self-center">
-        P1
-      </span>
-      <span className="col-start-1 row-start-3 content-center justify-self-center">
-        P2
-      </span>
-      <span className="col-start-1 row-start-4 content-center justify-self-center">
-        P3
-      </span>
-      <span className="col-start-1 row-start-5 content-center justify-self-center">
-        P4
-      </span>
-      <span className="col-start-2 row-start-1 place-self-center">LFO</span>
-      <span className="col-start-3 row-start-1 place-self-center">RND</span>
-      <span className="col-start-4 row-start-1 place-self-center">SEQ</span>
-      <span className="col-start-5 row-start-1 place-self-center">VEL</span>
+      {rowLables.map((label, i) => (
+        <span
+          key={`row-label-${i + 1}`}
+          className={`row-start-${i + 2} content-center justify-self-center`}
+        >
+          {label}
+        </span>
+      ))}
+
+      {/* spacer */}
       <span className="col-start-1 row-start-1 place-self-center"></span>
+
+      {colLables.map((col, i) => (
+        <span
+          key={`col-col-${i + 1}`}
+          className={`col-start-${i + 2} row-start-1 place-self-center`}
+        >
+          {col}
+        </span>
+      ))}
+
       {modCells.map((cell, i) => (
         <ModCell
           key={i}

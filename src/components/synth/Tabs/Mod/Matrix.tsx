e179ad7e4dcdfa35037724543ec84fcfeac74179
vybukhivka <viktorbakhovka@gmail.com>
2025-01-13 17:39:43 +0200

    mod matrix titles

--- src/components/synth/Tabs/Mod/Matrix.tsx
+++ src/components/synth/Tabs/Mod/Matrix.tsx
@@ -3,14 +3,29 @@ import { selectMatrix } from '../../../../store/slices/modulationSlice';
 import readMatrixValues from '../../../../utils/readMatrixValues';
 import ModCell from '../../../ui/ModCell/ModCell';
 
-// const matrixSlots: number[] = Array.from({ length: 16 }, (_, i) => i);
-
 const Matrix: React.FC = ({ selectedTrack }) => {
   const matrix = useAppSelector(selectMatrix);
   const modCells = readMatrixValues('track1', matrix);
 
   return (
-    <div className="grid h-[200px] w-[200px] grid-cols-4 grid-rows-4 border-slate-600">
+    <div className="mr-9 grid h-[250px] w-[250px] grid-cols-5 grid-rows-5 border-slate-600">
+      <span className="col-start-1 row-start-2 content-center justify-self-center">
+        P1
+      </span>
+      <span className="col-start-1 row-start-3 content-center justify-self-center">
+        P2
+      </span>
+      <span className="col-start-1 row-start-4 content-center justify-self-center">
+        P3
+      </span>
+      <span className="col-start-1 row-start-5 content-center justify-self-center">
+        P4
+      </span>
+      <span className="col-start-2 row-start-1 place-self-center">LFO</span>
+      <span className="col-start-3 row-start-1 place-self-center">RND</span>
+      <span className="col-start-4 row-start-1 place-self-center">SEQ</span>
+      <span className="col-start-5 row-start-1 place-self-center">VEL</span>
+      <span className="col-start-1 row-start-1 place-self-center"></span>
       {modCells.map((cell, i) => (
         <ModCell
           key={i}
